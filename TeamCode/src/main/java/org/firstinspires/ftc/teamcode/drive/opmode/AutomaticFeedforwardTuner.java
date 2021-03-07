e5d93ab0e8ac60e048fa7ece5037921e2a8b04bb
Adrian Chan <adrian27513@gmail.com>
2021-03-07 17:18:00 -0500

    Changed some measurements

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/AutomaticFeedforwardTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/AutomaticFeedforwardTuner.java
@@ -35,7 +35,7 @@ import static org.firstinspires.ftc.teamcode.drive.DriveConstants.rpmToVelocity;
 @Autonomous(group = "drive")
 public class AutomaticFeedforwardTuner extends LinearOpMode {
     public static double MAX_POWER = 0.7;
-    public static double DISTANCE = 100; // in
+    public static double DISTANCE = 70; // in
 
     @Override
     public void runOpMode() throws InterruptedException {
