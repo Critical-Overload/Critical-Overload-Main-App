2da0c37edb7cb4e2af28122b08e64a08151c1ea2
Adrian Chan <adrian27513@gmail.com>
2021-03-07 17:00:01 -0500

    Changed some measurements

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -31,7 +31,7 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double WHEEL_RADIUS = (1.49606/2); // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = 14.5; // in; distance between the left and right wheels
+    public static double LATERAL_DISTANCE = 14.50333; // in; distance between the left and right wheels
     public static double FORWARD_OFFSET = -8.5; // in; offset of the lateral wheel
 
     private Encoder leftEncoder, rightEncoder, frontEncoder;
