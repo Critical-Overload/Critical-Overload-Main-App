b8e9bf34a8e0db80f832e78af745aeb68989e124
criti <critical.overlord@gmail.com>
2021-04-10 18:02:12 -0400

    Adrian's odometry stuff + main

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/LocalizationTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/LocalizationTest.java
@@ -1,9 +1,9 @@
 package org.firstinspires.ftc.teamcode.drive.opmode;
 
-import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
@@ -14,18 +14,24 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
  * exercise is to ascertain whether the localizer has been configured properly (note: the pure
  * encoder localizer heading may be significantly off if the track width has not been tuned).
  */
+
 @TeleOp(group = "drive")
 public class LocalizationTest extends LinearOpMode {
+
+
     @Override
     public void runOpMode() throws InterruptedException {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
         drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
+
+
         waitForStart();
 
-        while (!isStopRequested()) {
-            drive.setWeightedDrivePower(
+
+       while (!isStopRequested()) {
+           drive.setWeightedDrivePower(
                     new Pose2d(
                             -gamepad1.left_stick_y,
                             -gamepad1.left_stick_x,
