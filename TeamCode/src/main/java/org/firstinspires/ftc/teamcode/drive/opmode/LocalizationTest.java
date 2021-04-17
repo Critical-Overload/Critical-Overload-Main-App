e24880dd414525e8dfbf33bf96e09e2d0fa3700b
Adrian Chan <adrian27513@gmail.com>
2021-04-17 16:58:55 -0400

    Changed some Mecanum stuff @ line 161 and 162 Added robotControl and Adrian MainTeleOp Edited AutonomousMain Disabled a bunch of Tuners and Testers

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/LocalizationTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/LocalizationTest.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.drive.opmode;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -14,24 +15,19 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
  * exercise is to ascertain whether the localizer has been configured properly (note: the pure
  * encoder localizer heading may be significantly off if the track width has not been tuned).
  */
-
+@Disabled
 @TeleOp(group = "drive")
 public class LocalizationTest extends LinearOpMode {
-
-
     @Override
     public void runOpMode() throws InterruptedException {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
         drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-
-
         waitForStart();
 
-
-       while (!isStopRequested()) {
-           drive.setWeightedDrivePower(
+        while (!isStopRequested()) {
+            drive.setWeightedDrivePower(
                     new Pose2d(
                             -gamepad1.left_stick_y,
                             -gamepad1.left_stick_x,
